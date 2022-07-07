465dadad3320c0e172c5a6e3988a111d240b66b6
owaistahir3 <owais.tahir@ideofuzion.com>
2022-07-07 16:46:30 +0500

    SOL Transfer Function

--- token/js/examples/TransferBack.ts
+++
@@ -1,6 +0,0 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
-import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '../src'; // @FIXME: replace with @solana/spl-token
-
-import toWallet from './create_mint_and_transfer_tokens';
-
-console.log('Check: ', toWallet);
--- token/js/examples/create_mint_and_transfer_tokens.ts
+++ token/js/examples/create_mint_and_transfer_tokens.ts
@@ -1,19 +1,37 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, Transaction, SystemProgram, sendAndConfirmTransaction } from '@solana/web3.js';
 import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '../src'; // @FIXME: replace with @solana/spl-token
 
-export default async () => {
+(async () => {
     // Connect to cluster
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
     // Generate a new wallet keypair and airdrop SOL
     const fromWallet = Keypair.generate();
     const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+    console.log('Airdrop tx:', fromAirdropSignature);
 
     // Wait for airdrop confirmation
     await connection.confirmTransaction(fromAirdropSignature);
 
     // Generate a new wallet to receive newly minted token
     const toWallet = Keypair.generate();
+
+    // Transfering some SOL to the new wallet
+    const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: fromWallet.publicKey,
+          toPubkey: toWallet.publicKey,
+          lamports: LAMPORTS_PER_SOL / 2,
+        }),
+      );
+    
+      // Sign transaction, broadcast, and confirm
+      let signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [fromWallet],
+      );
+      console.log('SIGNATURE', signature);
 
     // Create new token mint
     const mint = await createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
@@ -30,7 +48,7 @@ export default async () => {
     const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey);
 
     // Mint 1 new token to the "fromTokenAccount" account we just created
-    let signature = await mintTo(
+    signature = await mintTo(
         connection,
         fromWallet,
         mint,
@@ -52,4 +70,16 @@ export default async () => {
         []
     );
     console.log('transfer tx:', signature);
-};
+
+     // Transfer the token back to the "fromTokenAccount" we just created
+     signature = await transfer(
+        connection,
+        toWallet,
+        toTokenAccount.address,
+        fromTokenAccount.address,
+        toWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log('transferback tx:', signature);
+})();
